0a368b19e52d396b9465eb939e0392930b8303a2
merkrehan <mrkrehanishaq1990@gmail.com>
2014-07-25 02:29:33 +0200

    follow the new versioning system
    Change-Id: Ib187a22a7f54d059de22ef7e663db686534dab37

--- config/version.mk
+++ config/version.mk
@@ -1,31 +1,35 @@
 # Versioning of the ROM
 
-ifdef BUILDTYPE_AUTOTEST
-    ROM_BUILDTYPE := AUTOTEST
-endif
-ifdef BUILDTYPE_RELEASE
-    ROM_BUILDTYPE := RELEASE
-endif
-
-ifndef ROM_BUILDTYPE
-    ROM_BUILDTYPE := TESTS
-endif
+ MERK_MAJOR := 1
+ MERK_MINOR := 0
+ MERK_VERSION_MAINTENANCE := 1
+ MERK_VERSION_TAG := BETA
+ MERK_BUILDTYPE :=
 
 TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
 TARGET_PRODUCT_SHORT := $(subst merk_,,$(TARGET_PRODUCT_SHORT))
 
 # Build the final version string
 ifdef BUILDTYPE_RELEASE
-    ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
+ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
+else
+ifneq ($(ROM_BUILDTIME_CUSTOM),)
+DATE := $(ROM_BUILDTIME_CUSTOM)
 else
 ifeq ($(ROM_BUILDTIME_LOCAL),y)
-    ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d-%H%M%z)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
+DATE := $(shell date +%Y%m%d-%H%M%z)
 else
-    ROM_VERSION := $(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
+DATE := $(shell date -u +%Y%m%d)
 endif
+endif # BUILDTYPE_RELEASE
+
+ROM_VERSION := $(MERK_MAJOR).$(MERK_MINOR)-$(TARGET_PRODUCT_SHORT)-$(MERK_VERSION_TAG)-$(MERK_VERSION_MAINTENANCE)
 endif
+
+MERK_VERSION := $(ROM_VERSION)-$(shell date -u +%Y%m%d)
 
 # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=merkMod-$(ROM_VERSION) \
-    ro.merk.version=$(ROM_VERSION)
+ro.modversion=MerkMod-$(ROM_VERSION) \
+ro.merk.version=$(MERK_VERSION) \
+ro.merk.date=$(shell date +"%Y%m%d")
