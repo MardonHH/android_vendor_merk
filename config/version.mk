9be259cd065a38efa0aa67d7d4b5ca15203c5ed2
merkrehan <mrkrehanishaq1990@gmail.com>
2014-09-05 22:53:50 +0530

    start jenkins & experiment
    Change-Id: I2b957f3dd424f3cee35de8ffdfaa69433737a1f2

--- config/version.mk
+++ config/version.mk
@@ -1,35 +1,55 @@
 # Versioning of the ROM
 
- MERK_MAJOR := 1
- MERK_MINOR := 0
- MERK_VERSION_MAINTENANCE := 3
- MERK_VERSION_TAG := beta
- MERK_BUILDTYPE := Test
+ifeq ($(BUILD_IS_JENKINS),1)
+
+ifneq ($(BUILD_BUILDTYPE),)
+    ROM_BUILDTYPE = $(BUILD_BUILDTYPE)
+else
+    ROM_BUILDTYPE := NIGHTLY
+endif
+
+endif # we are jenkins
+
+ifdef BUILDTYPE_RELEASE
+    ROM_BUILDTYPE := RELEASE
+endif
+
+ifndef ROM_BUILDTYPE
+    ROM_BUILDTYPE := EXPERIMENTAL
+endif
 
 TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
 TARGET_PRODUCT_SHORT := $(subst merk_,,$(TARGET_PRODUCT_SHORT))
 
 # Build the final version string
 ifdef BUILDTYPE_RELEASE
-ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
+    ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
 else
 ifneq ($(ROM_BUILDTIME_CUSTOM),)
-DATE := $(ROM_BUILDTIME_CUSTOM)
+    DATE := $(ROM_BUILDTIME_CUSTOM)
 else
 ifeq ($(ROM_BUILDTIME_LOCAL),y)
-DATE := $(shell date +%Y%m%d-%H%M%z)
+    DATE := $(shell date +%Y%m%d-%H%M%z)
 else
-DATE := $(shell date -u +%Y%m%d)
+    DATE := $(shell date -u +%Y%m%d)
 endif
 endif # BUILDTYPE_RELEASE
 
-ROM_VERSION := $(MERK_MAJOR).$(MERK_MINOR)-$(TARGET_PRODUCT_SHORT)-$(MERK_VERSION_TAG).$(MERK_VERSION_MAINTENANCE)
+ROM_VERSION := $(PLATFORM_VERSION)-$(DATE)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
 endif
 
-MERK_VERSION := $(ROM_VERSION)-$(shell date -u +%Y%m%d)
+MERK_VERSION := $(ROM_VERSION)
 
 # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.modversion=MerkMod-$(ROM_VERSION) \
-ro.merk.version=$(MERK_VERSION) \
-ro.merk.date=$(shell date +"%Y%m%d")
+    ro.modversion=merk-$(ROM_VERSION) \
+    ro.merk.version=$(ROM_VERSION) \
+    ro.merk.date=$(shell date +"%Y%m%d") \
+    ro.merk.releasetype=$(ROM_BUILDTYPE) \
+
+# disable multithreaded dextop for everything but nightlies and homemade
+ifeq ($(filter MERK EXPERIMENTAL,$(ROM_BUILDTYPE)),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dalvik.multithread=false
+endif
+
